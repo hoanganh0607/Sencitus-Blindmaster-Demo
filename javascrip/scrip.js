84b35769e8976d1adef243d430df4d87445033b4
hoanganh0607 <hoanganhn39@gmail.com>
2020-01-07 17:09:45 +0700

    them

--- javascrip/scrip.js
+++ javascrip/scrip.js
@@ -1,13 +1,60 @@
 $(document).ready(function () {
     $('.slick-banner').slick();
 
-    $('.slick-img').slick({
+    $('.slick-img').slick({       
         slidesToShow: 6,
         slidesToScroll: 1,
-        autoplay: true,
         autoplaySpeed: 2000,
-
+        responsive: [
+            {
+              breakpoint: 1500,
+              settings: {
+                slidesToShow: 5,
+                slidesToScroll: 1,
+                
+              }
+            },
+            {
+                breakpoint: 1200,
+                settings: {
+                  slidesToShow: 4,
+                  slidesToScroll: 1
+                }
+              },
+            {
+              breakpoint: 1000,
+              settings: {
+                slidesToShow: 4,
+                slidesToScroll: 1,
+                infinite: true,
+              }
+            },
+            {
+              breakpoint: 845,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+              }
+            }
+          ]
     })
+
+    /*menu fixed*/
+
+    $(window).on('scroll', function() {
+      if ($(window).scrollTop()) {
+        $('.header-top').addClass('header-top-fixed');
+      } else {
+        $('.header-top').removeClass('header-top-fixed');
+      }
+    });
 
     /*menu mobile*/
 
@@ -16,9 +63,7 @@ $(document).ready(function () {
     $(".icon-click-show").click(function () {
         $(".menu-mobile").toggle();
 
-    })
-
-    
+    })  
 
     $('.outdoor').hide()
     $('#menu-back').hide()
